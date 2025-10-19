9ceb4c22d0444b00345a78ba79f0a3cb6b864d97
Jon Nordby <jononor@gmail.com>
2025-10-19 15:04:55 +0200

    sensor_reader: Actually extract/process window chunks

--- samples/modules/emlearn/sensor_reader/tools/process.c
+++ samples/modules/emlearn/sensor_reader/tools/process.c
@@ -8,6 +8,9 @@
 #include "preprocessing.h"
 
 // Input format
+
+#define SENSOR_DATA_COLUMNS 6
+
 const char *expect_columns[7] = {
     "time",
     "acc_x",
@@ -22,7 +25,7 @@ char *input_columns[INPUT_COLUMNS_MAX];
 float input_values[INPUT_COLUMNS_MAX];
 
 // Output format
-// WARNING: length must match
+// WARNING: length of values must match
 #define OUTPUT_COLUMNS_LENGTH (accelgyro_features_length)
 const char *output_columns[OUTPUT_COLUMNS_LENGTH] = {
     "time",
@@ -39,8 +42,15 @@ float output_values[OUTPUT_COLUMNS_LENGTH];
 
 
 // Working buffers
-#define READ_BUFFER_SIZE 1024
+
+// Read buffer for CSV row
+// Can easily be 10x the number of columns, because of textual encoding of floating-point numbers
+#define READ_BUFFER_SIZE 1000
 char read_buffer[READ_BUFFER_SIZE];
+
+// Sensor data window
+#define WINDOW_LENGTH_MAX 100
+float window_buffer[WINDOW_LENGTH_MAX*SENSOR_DATA_COLUMNS];
 
 
 // WARN: uses errno, not reentrant/threadsafe
@@ -57,6 +67,90 @@ int parse_integer(const char *str, long *out)
     *out = val;
     return 0;
 }
+
+
+// On success, returns the number of rows read. Normally @window_length, but might be shorter at end-of-input
+// On error, returns a negative integer
+int
+read_overlapped_windows(EmlCsvReader *reader,
+        int *read_index,
+        int window_length,
+        int hop_length,
+        float *buffer,
+        int buffer_length
+    )
+{
+    const int out_columns = SENSOR_DATA_COLUMNS;
+
+    // Check inputs
+    if (buffer_length != window_length*out_columns) {
+        //fprintf(stderr, "b=%d e=%d \n", buffer_length, window_length*out_columns);
+        return -12;
+    }
+
+    if (reader->n_columns != out_columns+1) {
+        fprintf(stderr, "b=%d e=%d \n", (int)reader->n_columns, out_columns+1);
+        return -3;
+    }
+
+    // Check invariants
+    if (*read_index > window_length) {
+        // read_index should go from 0 to window_length
+        return -2;
+    }
+    if (*read_index < 0) {
+        return -2;
+    }
+    if (*read_index > (buffer_length/out_columns)) {
+        return -2;
+    }
+
+    if (*read_index == window_length) {
+        // previous execution hit a full window
+        // prepare for new values coming in, by shifting existing data down
+        memcpy(buffer, buffer+(hop_length*out_columns), sizeof(float)*out_columns*window_length);
+        *read_index -= hop_length;
+    }
+
+    // Check if we need
+    const int max_rows = window_length;
+    int row = -1;
+    for (row=0; row<max_rows; row++) {
+        const int values_read = eml_csv_reader_read_data(reader,\
+            read_buffer, READ_BUFFER_SIZE, input_columns, INPUT_COLUMNS_MAX);
+        if (values_read == 0) {
+            // input stream finished, give partial window read
+            return *read_index;
+        }
+
+        // Parse as numbers
+        for (int i=0; i<reader->n_columns; i++) {
+            const float v = strtod(input_columns[i], NULL);
+            input_values[i] = v;
+        }
+
+        // NOTE: first column is time, ignored
+        // TODO: generalize or move this logic elsewhere?
+        const float *values = input_values+1;
+
+        // Buffer received data
+        const int read_offset = ((*read_index)*out_columns);
+        memcpy(buffer+read_offset, values, sizeof(float)*out_columns);
+        *read_index += 1;
+
+        if (*read_index == window_length) {
+            // A full window, signal
+            return *read_index;
+            // NOTE: rotating the buffer is done on next function call
+        }
+
+    }
+
+    // still waiting for a full window
+    return 0;
+}
+
+
 
 int
 main(int argc, const char *argv[])
@@ -75,18 +169,15 @@ main(int argc, const char *argv[])
         return -1;
     }
 
-    // Setup file input and output
+    const int window_length = 50; // FIXME: unhardcode
+    const int hop_length = 8; // FIXME: unhardcode
+
+    // Setup file input
     FILE *read_file = fopen(input_path, "r");
     if (read_file == NULL) {
         fprintf(stderr, "failed to open input\n");
         return -1;
     }
-    FILE *write_file = fopen(output_path, "w");
-    if (write_file == NULL) {
-        fprintf(stderr, "failed to open output\n");
-        return -1;
-    }
-
     EmlCsvReader _reader = {
         .seek = eml_fileio_seek,
         .read = eml_fileio_read,
@@ -94,31 +185,18 @@ main(int argc, const char *argv[])
     };
     EmlCsvReader *reader = &_reader;
     
-    EmlCsvWriter _writer = {
-        .n_columns = OUTPUT_COLUMNS_LENGTH,
-        .write = eml_fileio_write,
-        .stream = write_file,
-    };
-    EmlCsvWriter *writer = &_writer;
-
-    const EmlError write_header_err = \
-        eml_csv_writer_write_header(writer, output_columns, OUTPUT_COLUMNS_LENGTH);
-    if (write_header_err != EmlOk) {
-        fprintf(stderr, "header-write-fail error=%d \n", write_header_err);
-        return 1;
-    }
-
     // Check input header is as expected
     const EmlError read_header_err = eml_csv_reader_read_header(reader,\
         read_buffer, READ_BUFFER_SIZE, input_columns, INPUT_COLUMNS_MAX);
     if (read_header_err != EmlOk) {
         return 2;
     }
-    const int n_expect_columns = 7;
+    const int n_expect_columns = SENSOR_DATA_COLUMNS + 1; // sensor columns + time
     if (reader->n_columns != n_expect_columns) {
         return 2;
     }
 
+    // Check that columns are as expected
     for (int i=0; i<reader->n_columns; i++) {
         const bool correct = strcmp(input_columns[i], expect_columns[i]) == 0;
         if (!correct) {
@@ -128,53 +206,94 @@ main(int argc, const char *argv[])
         }
     }
 
+    // Setup file output
+    FILE *write_file = fopen(output_path, "w");
+    if (write_file == NULL) {
+        fprintf(stderr, "failed to open output\n");
+        return -1;
+    }
+
+    EmlCsvWriter _writer = {
+        .n_columns = OUTPUT_COLUMNS_LENGTH,
+        .write = eml_fileio_write,
+        .stream = write_file,
+    };
+    EmlCsvWriter *writer = &_writer;
+
+    // Write output header
+    const EmlError write_header_err = \
+        eml_csv_writer_write_header(writer, output_columns, OUTPUT_COLUMNS_LENGTH);
+    if (write_header_err != EmlOk) {
+        fprintf(stderr, "header-write-fail error=%d \n", write_header_err);
+        return -1;
+    }
+
+    if (window_length > WINDOW_LENGTH_MAX) {
+        return -2;
+    }
+    const int window_buffer_length = window_length*SENSOR_DATA_COLUMNS;
+
     // Setup preprocessing
     struct accelgyro_preprocessor preprocessor;
 
-    // Read and process data
-    const int max_rows = 10000;
-    int row = -1;
-    for (row=0; row<max_rows; row++) {
-        const int values_read = eml_csv_reader_read_data(reader,\
-            read_buffer, READ_BUFFER_SIZE, input_columns, INPUT_COLUMNS_MAX);
-        if (values_read == 0) {
-            // finished
+    // Processing loop
+    int window_no = 0;
+    int window_read_index = 0;
+    while (1) {
+
+        // Read input data
+        const int window_read = read_overlapped_windows(reader, &window_read_index,
+            window_length, hop_length,
+            window_buffer, window_buffer_length);
+
+        fprintf(stdout, "main-read-window window=%d index=%d \n", window_no, window_read_index);
+
+        if (window_read == window_length) {
+            // Complete window, process it
+            window_no += 1;
+
+            const int preprocess_err = \
+                accelgyro_preprocessor_run(&preprocessor, window_buffer, window_buffer_length);
+            if (preprocess_err != 0) {
+                fprintf(stderr, "preprocessor error %d\n", preprocess_err);
+                return -3;
+            }
+
+            // Provide time as output
+            const float hop_duration = hop_length / (float)samplerate;
+            output_values[0] = window_no * hop_duration;
+
+            // Provide extracted features as output
+            for (int i=0; i<accelgyro_features_length; i++) {
+                output_values[i+1] = preprocessor.features[i];
+            }
+
+            // Write output values to file
+            const EmlError write_err = \
+                eml_csv_writer_write_data(writer, output_values, OUTPUT_COLUMNS_LENGTH);
+            if (write_err != EmlOk) {
+                fprintf(stderr, "failed to write output\n");
+                return -3;
+            }
+
+        } else if (window_read < 0) {
+            // error
+            fprintf(stderr, "window read error %d\n", window_read);
             break;
+        } else if (window_read != 0) {
+            // partial window, typically at end-of-input. Ignored for now
+            break;
+        } else {
+            // incomplete window, do nothing - wait for it to become complete
         }
 
-        // Parse as numbers
-        for (int i=0; i<reader->n_columns; i++) {
-            const float v = strtod(input_columns[i], NULL);
-            input_values[i] = v;
-        }
-
-        // FIXME: need to batch up N rows of sensor data
-        // Run through preprocessor
-        const int hop_length = 1;
-        const float *sensor_data = input_values+1; // first column is time, ignored
-        const int window_samples_total = hop_length * 6;
-
-        accelgyro_preprocessor_run(&preprocessor, sensor_data, window_samples_total);
-
-        // FIXME: compute output time based on the window
-        output_values[0] = input_values[0];
-        for (int i=0; i<accelgyro_features_length; i++) {
-            output_values[i+1] = preprocessor.features[i];
-        }
-
-        // Write output values
-        const EmlError write_err = \
-            eml_csv_writer_write_data(writer, output_values, OUTPUT_COLUMNS_LENGTH);
-        if (write_err != EmlOk) {
-            fprintf(stderr, "failed to write output\n");
-            return 2;
-        }
     }
 
     fclose(write_file);
     fclose(read_file);
 
-    printf("main-done rows=%d \n", row);
+    const float last_timestamp = output_values[0];
+    printf("main-done windows=%d time=%.3f\n", window_no, last_timestamp);
 
     return 0;
 }
